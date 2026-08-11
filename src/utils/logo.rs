bfa1d5985888f2e587acf76f99242071b3611450
eetu-niittymaki <eetu945@gmail.com>
2026-08-11 16:38:58 +0300

    Add cmd support for logo

--- src/utils/logo.rs
+++ src/utils/logo.rs
@@ -1,6 +1,9 @@
 use owo_colors::OwoColorize;
 
 pub fn logo() {
+    #[cfg(windows)]
+    enable_ansi_support::enable_ansi_support().ok();
+    
     const LOGO: &str = 
     r#"                                    
 ███▄▄  ███  ▄██████▄  █████████ ████████ ▄███████▄
